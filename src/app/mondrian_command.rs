312ecb9280c2e132ece51374dd3ef3327fb31099
Pasquale Policastro <40884503+policastro@users.noreply.github.com>
2024-11-05 23:43:14 +0100

    refactor(configs): replace action "module" with "pause"
    The action "module <keybindings|overlays>" was removed and now the same
    functionality can be achieved using "pause [keybindings|overlays]".
    
    BREAKING CHANGE: the action "module <keybindings|overlays>" was removed from the config options

--- src/app/mondrian_command.rs
+++ src/app/mondrian_command.rs
@@ -1,10 +1,10 @@
-use std::collections::HashSet;
-
-use serde::{Deserialize, Deserializer};
-
-use crate::modules::core::lib::tm_command::TMCommand;
-
 use super::structs::direction::Direction;
+use crate::modules::core::lib::tm_command::TMCommand;
+use serde::Deserialize;
+use serde::Deserializer;
+use serde::Serialize;
+use std::collections::HashSet;
+use std::str::FromStr;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum MondrianMessage {
@@ -39,16 +39,16 @@ impl<'de> serde::Deserialize<'de> for MondrianMessage {
             "minimize",
             "focus <left|right|up|down>",
             "move <left|right|up|down>",
-            "resize <left|right|up|down> <40-250> ",
+            "resize <left|right|up|down> <40-250>",
             "invert",
             "release",
             "focalize",
-            "pause",
-            "module <keybindings|overlays>",
+            "pause [keybindings|overlays]",
             "quit",
         ];
 
         let s: String = Deserialize::deserialize(d)?;
+        let s = s.to_lowercase();
         let parts: Vec<&str> = s.split(' ').map(|s| s.trim()).collect();
 
         let valid_len = match parts[0] {
@@ -62,17 +62,14 @@ impl<'de> serde::Deserialize<'de> for MondrianMessage {
             "invert" => parts.len() == 1,
             "release" => parts.len() == 1,
             "focalize" => parts.len() == 1,
-            "pause" => parts.len() == 1,
-            "module" => parts.len() <= 2,
+            "pause" => parts.len() <= 2,
             "quit" => parts.len() == 1,
             _ => false,
         };
 
-        let msg = format!("Invalid action: {}, valid actions are: {:?}", s, actions.join(", "));
-        let invalid_action_err: Result<Self, D::Error> = Err(serde::de::Error::custom(msg));
-
+        let err = format!("Invalid action: {}, valid actions are: {:?}", s, actions.join(", "));
         if !valid_len {
-            return invalid_action_err;
+            return Err(serde::de::Error::custom(err.clone()));
         }
 
         match parts[0] {
@@ -81,58 +78,58 @@ impl<'de> serde::Deserialize<'de> for MondrianMessage {
             "retile" => Ok(MondrianMessage::Retile),
             "minimize" => Ok(MondrianMessage::Minimize),
             "focus" => {
-                let dir = match parts[1] {
-                    "up" => Direction::Up,
-                    "down" => Direction::Down,
-                    "left" => Direction::Left,
-                    "right" => Direction::Right,
-                    _ => return invalid_action_err,
-                };
+                let dir = Direction::from_str(parts[1]).map_err(|_| serde::de::Error::custom(err))?;
                 Ok(MondrianMessage::Focus(dir))
             }
             "move" => {
-                let dir = match parts[1] {
-                    "up" => Direction::Up,
-                    "down" => Direction::Down,
-                    "left" => Direction::Left,
-                    "right" => Direction::Right,
-                    _ => return invalid_action_err,
-                };
+                let dir = Direction::from_str(parts[1]).map_err(|_| serde::de::Error::custom(err))?;
                 Ok(MondrianMessage::Move(dir))
             }
             "resize" => {
-                let dir = match parts[1] {
-                    "up" => Direction::Up,
-                    "down" => Direction::Down,
-                    "left" => Direction::Left,
-                    "right" => Direction::Right,
-                    _ => return invalid_action_err,
-                };
-                let size: u8 = match parts[2].parse() {
-                    Ok(s) => s,
-                    Err(_) => return invalid_action_err,
-                };
-                if !(40..=250).contains(&size) {
-                    return invalid_action_err;
+                let dir = Direction::from_str(parts[1]).map_err(|_| serde::de::Error::custom(err.clone()))?;
+                let size: u8 = parts[2].parse().map_err(|_| serde::de::Error::custom(err.clone()))?;
+                match (40..=250).contains(&size) {
+                    true => Ok(MondrianMessage::Resize(dir, size)),
+                    false => Err(serde::de::Error::custom(err)),
                 }
-                Ok(MondrianMessage::Resize(dir, size))
             }
             "invert" => Ok(MondrianMessage::Invert),
             "focalize" => Ok(MondrianMessage::Focalize),
             "release" => Ok(MondrianMessage::Release(None)),
-            "pause" => Ok(MondrianMessage::Pause(None)),
-            "module" => {
-                let name = match parts.get(1).to_owned() {
-                    Some(&"keybindings") => "keybindings",
-                    Some(&"overlays") => "overlays",
-                    _ => Err(serde::de::Error::custom(
-                        "Invalid parameter for 'module', expected 'keybindings' or 'overlays'",
-                    ))?,
+            "pause" => {
+                let command = match parts.get(1).to_owned() {
+                    Some(v) if *v == "keybindings" => MondrianMessage::PauseModule(v.to_lowercase(), None),
+                    Some(v) if *v == "overlays" => MondrianMessage::PauseModule(v.to_lowercase(), None),
+                    _ => MondrianMessage::Pause(None),
                 };
-                Ok(MondrianMessage::PauseModule(name.to_lowercase(), None))
+                Ok(command)
             }
             "quit" => Ok(MondrianMessage::Quit),
-            _ => invalid_action_err,
+            _ => Err(serde::de::Error::custom(err)),
+        }
+    }
+}
+
+impl Serialize for MondrianMessage {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match self {
+            MondrianMessage::RefreshConfig => serializer.serialize_str("refresh-config"),
+            MondrianMessage::OpenConfig => serializer.serialize_str("open-config"),
+            MondrianMessage::Retile => serializer.serialize_str("retile"),
+            MondrianMessage::Minimize => serializer.serialize_str("minimize"),
+            MondrianMessage::Focus(dir) => serializer.serialize_str(&format!("focus {}", dir)),
+            MondrianMessage::Move(dir) => serializer.serialize_str(&format!("move {}", dir)),
+            MondrianMessage::Resize(dir, size) => serializer.serialize_str(&format!("resize {} {}", dir, size)),
+            MondrianMessage::Invert => serializer.serialize_str("invert"),
+            MondrianMessage::Release(_) => serializer.serialize_str("release"),
+            MondrianMessage::Focalize => serializer.serialize_str("focalize"),
+            MondrianMessage::Pause(_) => serializer.serialize_str("pause"),
+            MondrianMessage::PauseModule(v, _) => serializer.serialize_str(&format!("pause {}", v)),
+            MondrianMessage::Quit => serializer.serialize_str("quit"),
+            _ => Err(serde::ser::Error::custom("Unsupported action")),
         }
     }
 }
