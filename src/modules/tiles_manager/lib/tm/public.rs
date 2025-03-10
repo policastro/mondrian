bd8611f5b453207167e7e736eb9584cb93a1be3e
Pasquale Policastro <40884503+policastro@users.noreply.github.com>
2025-03-10 22:56:32 +0100

    perf: limit focus history to avoid uncontrolled memory growth

--- src/modules/tiles_manager/lib/tm/public.rs
+++ src/modules/tiles_manager/lib/tm/public.rs
@@ -19,6 +19,7 @@ use crate::win32::window::window_obj::WindowObjHandler;
 use crate::win32::window::window_obj::WindowObjInfo;
 use crate::win32::window::window_ref::WindowRef;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use winvd::get_current_desktop;
 use winvd::Desktop;
 
@@ -415,16 +416,22 @@ impl TilesManagerOperations for TilesManager {
     }
 }
 
-#[derive(Default, Clone)]
+#[derive(Default, Clone, Debug)]
 pub struct FocusHistory {
     map: HashMap<WindowRef, u64>,
+    order: VecDeque<WindowRef>,
+    order_map: HashMap<WindowRef, usize>,
     current_max: u64,
 }
 
 impl FocusHistory {
+    const MAX_ENTRIES: usize = 100; // TODO: maybe should be configurable?
+
     pub fn new() -> Self {
         Self {
             map: HashMap::new(),
+            order: VecDeque::new(),
+            order_map: HashMap::new(),
             current_max: 0,
         }
     }
@@ -434,16 +441,27 @@ impl FocusHistory {
     }
 
     pub fn update(&mut self, window: WindowRef) {
-        // INFO: This should pratically never happen
+        // INFO: this should pratically never happen
         if self.current_max == u64::MAX {
-            self.clear();
+            let min_v = *self.map.values().min().unwrap_or(&0);
+            self.map.values_mut().for_each(|v| *v -= min_v);
+            self.current_max -= min_v;
         }
-        self.current_max += 1;
-        self.map.insert(window, self.current_max);
-    }
 
-    pub fn clear(&mut self) {
-        self.map.clear();
-        self.current_max = 0;
+        if let Some(p) = self.order_map.get(&window) {
+            self.order.remove(*p);
+        }
+
+        self.current_max += 1;
+        self.order.push_back(window);
+        self.order_map.insert(window, self.order.len() - 1);
+        self.map.insert(window, self.current_max);
+
+        // INFO: prevent map from growing indefinitely
+        if self.order.len() > Self::MAX_ENTRIES {
+            let w = self.order.pop_front().unwrap();
+            self.map.remove(&w);
+            self.order_map.remove(&w);
+        }
     }
 }
